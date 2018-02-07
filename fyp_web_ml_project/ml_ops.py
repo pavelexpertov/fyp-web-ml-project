b9b1877ff0b6fac7840cf8a04c8ea670c215c742
pavelexpertov <pavelm01@hotmail.co.uk>
2018-02-07 13:14:19 +0000

    Amend ml_ops functions

--- fyp_web_ml_project/ml_ops.py
+++ fyp_web_ml_project/ml_ops.py
@@ -10,9 +10,13 @@ def show_class_dict():
 
 def get_ml_class_list():
     '''Return a list of strings representing ML class names'''
-    return ml_class_dict.keys()
+    return list(ml_class_dict.keys())
 
 
 def get_ml_class_settings(ml_class_name):
     '''Return a machine learning algorithm settings in JSON format'''
-    ml_class_dict['ml_class_name']
+    try:
+        return ml_class_dict[ml_class_name]("get settings object").get_current_settings_parameters()
+    except KeyError:
+        raise Exception("{0} is not in the ML class list".format(ml_class_name))
+#def get_prediction_from_model(ml_model_id, features_values_json)
