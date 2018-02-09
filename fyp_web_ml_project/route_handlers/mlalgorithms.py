d9513bd7fb14684a13a60fc5ac0350109cf56925
pavelexpertov <pavelm01@hotmail.co.uk>
2018-02-09 01:02:52 +0000

    Make get_class_and_settings_dict return an appropriate json document

--- fyp_web_ml_project/route_handlers/mlalgorithms.py
+++ fyp_web_ml_project/route_handlers/mlalgorithms.py
@@ -1,4 +1,5 @@
 import fyp_web_ml_project.ml_ops as ml_ops
 
 def get_class_and_settings_dict():
-    return dict(ml_ops.get_ml_class_and_settings_list)
+    return [{'name': cls_name, 'propert_dict': property_dict}
+            for cls_name, property_dict in ml_ops.get_ml_class_and_settings_list()]
