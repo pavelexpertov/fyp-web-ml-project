7b84f7b7d1dc2cdd0f288a9ddb061cadd6691420
pavelexpertov <pavelm01@hotmail.co.uk>
2018-02-09 01:18:25 +0000

    Update the app.py routes methods.

--- fyp_web_ml_project/app.py
+++ fyp_web_ml_project/app.py
@@ -1,5 +1,5 @@
-from flask import Flask, request
-from .route_handlers import test
+from flask import Flask, request, jsonify
+from .route_handlers import mlmodels, dataset, mlalgorithms
 
 app = Flask(__name__)
 app.config.update(dict(
@@ -16,37 +16,51 @@ def serve_spa():
 @app.route('/mlmodels/<ml_id>', methods=['PUT', 'POST'])
 def train_model(ml_id):
     '''Machine learning model routes'''
+    json_body = request.get_json(force=True)
     if request.method == 'POST':
-        return "You just passed " + ml_id + " via POST"
+        mlmodels.create_model(ml_id, json_body['ml_class_name'], json_body['settings_json'], json_body['dataset_query_json'])
+        obj = {'ok': True, 'message': "{0} has been created".format(ml_id)}
+        return jsonify(obj)
+    elif request.method == 'PUT':
+        mlmodels.reconfigure_model(ml_id, json_body['settings_json'], json_body['dataset_query_json'])
+        obj = {'ok': True, 'message': "{0} has been reconfigured".format(ml_id)}
+        return jsonify(obj)
     else:
-        return "You just passed " + ml_id + " via PUT"
+        raise Exception("The method you used is wrong")
 
 
 @app.route('/mlmodels/<ml_id>/predictions', methods=['GET', 'POST'])
 def predict_model(ml_id):
     '''Machine learning model routes'''
+    # Get a prediction
     if request.method == 'POST':
-        return "You just passed " + ml_id + " via POST"
+        features_values_json = request.get_json(force=True)
+        prediction = mlmodels.get_prediction_from_model(ml_id, features_values_json)
+        return jsonify({'prediction': prediction})
+    # Get a schema representing prediction features form
     else:
-        return "You just passed"
+        features_form = mlmodels.get_prediction_factors_form_from_model(ml_id)
+        return jsonify(features_form)
 
 
 @app.route('/mlmodels/<ml_id>/created')
 def is_model_create(ml_id):
     '''Machine learning model routes'''
-    return "You just passed"
+    obj = {'created': mlmodels.is_model_created(ml_id)}
+    return jsonify(obj)
 
 
 @app.route('/mlalgorithms')
 def get_ml_algorithms():
     '''Return a list of available algorithms and their information'''
-    return "Here's supposed to be some data"
+    return jsonify(mlalgorithms.get_class_and_settings_dict())
 
 
 @app.route('/dataset', methods=['POST'])
 def query_data_set():
     '''Return result for a dataset query'''
-    return "Here's supposed to be some data"
+    query_json = request.get_json(force=True)
+    return jsonify(dataset.query_data_set(query_json))
 
 
 @app.route('/add')
