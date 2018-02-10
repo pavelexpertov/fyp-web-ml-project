805adba00fc5b541e4f34ac3141f77ad39ef3bde
pavelexpertov <pavelm01@hotmail.co.uk>
2018-02-10 21:55:19 +0000

    Add initial implementation for views

---
+++ vue-ui/src/views/DataFeatureAnalysisView.vue
@@ -0,0 +1,43 @@
+<template>
+    <el-row>
+        <el-col :span="8">
+            <template v-if="!mlConfigObject">
+                <el-input v-model="newDataConfigName" placeholder="Enter a new name for Dataset configuration"></el-input>
+                <el-button @click="saveName">Save</el-button>
+            </template>
+            <data-set-configurations-panel
+            v-else
+            :datasetConfigObj="dataConfigObject"
+            :showButton="true"
+            >
+            </data-set-configurations-panel>
+        </el-col>
+        <el-col :span="18">
+        </el-col>
+    </el-row>
+</template>
+
+<script>
+import DataSetConfigurationsPanel from '@components/DataSetConfigurationsPanel'
+
+export default {
+    name: 'DataFeatureAnalysisView',
+    data () {
+        return {
+            dataConfigObject: ''
+        }
+    },
+    methods: {
+        saveName () {
+            //Add a default dataset configuration
+            console.log("Implement me in the savename of the datafeatureanalysis!!!")
+        }
+    },
+    components: {
+        dataSetConfigurationsPanel: DataSetConfigurationsPanel
+    }
+}
+</script>
+
+<style>
+</style>
---
+++ vue-ui/src/views/MlPlaygroundView.vue
@@ -0,0 +1,75 @@
+<template>
+    <div>
+        <el-row>
+        </el-row>
+        <el-row>
+            <template v-if="!mlConfigObject">
+                <el-input v-model="newMlConfigName" placeholder="Enter a new name for ML configuration"></el-input>
+                <el-button @click="saveName">Save</el-button>
+            </template>
+            <ml-configurations-panel
+            v-else
+            :mlConfigurationsObj="mlConfigObject"
+            >
+            </ml-configurations-panel>
+        </el-row>
+        <el-row>
+            <el-select v-model="dataConfigObject" placeholder="Select a dataconfig">
+                <el-option
+                v-for="data_config in datasetConfigsList"
+                :key="data_config.name"
+                :label="data_config.name"
+                :value="data_config">
+                </el-option>
+            </el-select>
+            <data-set-configurations-panel
+            v-if="dataConfigObject"
+            :datasetConfigObj="dataConfigObject"
+            :showButton="false"
+            >
+            </data-set-configurations-panel>
+        </el-row>
+    </div>
+</template>
+
+<script>
+import MlConfigurationsPanel from '@components/MlConfigurationsPanel'
+import DataSetConfigurationsPanel from '@components/DataSetConfigurationsPanel'
+
+export default {
+    name: "MlPlaygroundView",
+    data () {
+        return {
+            mlConfigObject: '',
+            dataConfigObject:'',
+            newMlConfigName: ''
+        }
+    },
+    components: {
+        mlConfigurationsPanel: MlConfigurationsPanel,
+        dataSetConfigurationsPanel: DataSetConfigurationsPanel
+    },
+    computed: {
+        datasetConfigsList: function(){
+            //Return a list of configs for the dropdown list
+            console.log("Implement me in the datasetConfigsList")
+        }
+    }
+    methods: {
+        saveName () {
+            //Create a new empty ML config object after user enters a name
+            console.log("Implement me in the saveName")
+        }
+    },
+    created: function(){
+        //check for the existing ml Config
+        let model_name = this.$route.params.model_name
+        if(model_name){
+            console.log("Implement me!!!!!!!")
+        }
+    }
+}
+</script>
+
+<style>
+</style>
