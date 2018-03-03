a95be35411903b775eea98aa19ff1c7fdf852510
pavelexpertov <pavelm01@hotmail.co.uk>
2018-03-03 20:35:40 +0000

    Add a new module for storing ml classes.

--- vue-ui/src/store/index.js
+++ vue-ui/src/store/index.js
@@ -74,9 +74,50 @@ let mlConfigModule = {
   }
 }
 
+let mlClassConfigModule = {
+  state: {
+    ml_class_configs_obj_list: []
+  },
+  mutations: {
+    addMlClassConfigObj (state, passedDict) {
+      if (!(passedDict.code && passedDict.class_name)) { console.log('passedDict needs to have class_name or/and code') } else { state.ml_class_configs_obj_list.push(passedDict) }
+    },
+    saveMlClassCode (state, passedDict) {
+      if (!(passedDict.code && passedDict.class_name)) { console.log('passedDict needs to have class_name or/and code') }
+      let list = state.ml_class_configs_obj_list
+      let index = _.findIndex(list, passedDict.class_name)
+      if (index !== -1) {
+        list[index].code = passedDict.code
+      } else {
+        console.log("Something's wrong in saveMlClassConfgObj")
+      }
+    }
+  },
+  getters: {
+    getMlClassConfigObjByName: (state) => (className) => {
+      let list = state.ml_class_configs_obj_list
+      let index = _.findIndex(list, {'class_name': className})
+      if (index !== -1) {
+        return list[index]
+      } else {
+        console.log('Something wrong in getMlClassConfigObjByName')
+        return null
+      }
+    },
+    getMlClassConfigObjNamesList: (state) => {
+      let list = state.ml_class_configs_obj_list
+      let namesList = []
+      _.forEach(list, function (value) {
+        namesList.push(value.class_name)
+      })
+    }
+  }
+}
+
 export default new Vuex.Store({
   modules: {
     ml: mlConfigModule,
-    data: dataConfigModule
+    data: dataConfigModule,
+    mlClass: mlClassConfigModule
   }
 })
