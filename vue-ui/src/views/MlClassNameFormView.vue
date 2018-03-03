66aec9699c7833c652bed6b94397523995281fdb
pavelexpertov <pavelm01@hotmail.co.uk>
2018-03-03 20:35:30 +0000

    Add view for adding new ml classes.

---
+++ vue-ui/src/views/MlClassNameFormView.vue
@@ -0,0 +1,42 @@
+<template>
+<div>
+    <h4>Enter a name for you class</h4>
+    <el-input v-model="mlClassName"></el-input>
+    <el-button @click="handleCreateClick">Create</el-button>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'MlClassNameFormView',
+  data () {
+    return {
+      mlClassName: ''
+    }
+  },
+  created () {
+
+  },
+  methods: {
+    handleCreateClick () {
+      const className = this.mlClassName
+      const initialCode = `
+import sklearn
+
+class ${className}(AbstractMlClass):
+    def __init__(self, name, settings_json=''):
+        '''Default constructor: the conditions must be the same to accomodate for default and
+        optional settings of the algorithm'''
+        if settings_json:
+            super().__init__(name, DecisionTreeRegressor(**settings_json))
+        else:
+            super().__init__(name, DecisionTreeRegressor())
+`
+      console.log(initialCode)
+    }
+  }
+}
+</script>
+
+<style>
+</style>
